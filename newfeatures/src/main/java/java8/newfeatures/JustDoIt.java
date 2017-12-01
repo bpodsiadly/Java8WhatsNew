a16477188a709be48ed2cddd78183be01e191b2d
bpodsiadly <bartek.podsiadly@gmail.com>
2017-12-01 16:57:17 +0100

    add new class to check some mistake

---
+++ newfeatures/src/main/java/java8/newfeatures/JustDoIt.java
@@ -0,0 +1,14 @@
+package java8.newfeatures;
+
+public class JustDoIt {
+
+	public static void main(String[] args) {
+		pleaseCheckIt();
+	}
+
+	public static void pleaseCheckIt() {
+		String oldOne = null;
+		boolean newOne = (oldOne != null) ? true : null;
+
+	}
+}
